b14cce9a7d349ddab1f54c4cb7288f9eb71c3000
dizhanm <dizhanm@gmail.com>
2025-02-12 10:37:54 +0500

    Delete lab4/date directory

--- lab4/date/1.py
+++
@@ -1,5 +0,0 @@
-import datetime
-x = datetime.datetime.now()
-y = x - datetime.timedelta(5)
-print(y)
-print(f"{y.day}.{y.month}.{y.year}")
--- lab4/date/2.py
+++
@@ -1,4 +0,0 @@
-import datetime
-print(datetime.datetime.now() - datetime.timedelta(1))
-print(datetime.datetime.now())
-print(datetime.datetime.now() + datetime.timedelta(1))
