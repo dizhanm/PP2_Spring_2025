8504d9579f6103a3a7b28876b3f78e98bfacf461
dizhanm <dizhanm@gmail.com>
2025-03-05 00:13:06 +0500

    Delete  lab6pp2/builtin directory

---  lab6pp2/builtin/1.py
+++
@@ -1,9 +0,0 @@
-def multiply_list(numbers):
-    result = 1
-    for num in numbers:
-        result *= num  
-    return result
-
-
-nums = [2, 3, 4, 5]
-print("Product of all numbers:", multiply_list(nums))
